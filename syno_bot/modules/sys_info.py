16cbb63ffabe1b441119c6d9217feef252d51481
Nicholas Chum <nicholas@prjkt.io>
2020-04-06 01:41:30 -0400

    SysInfo: Add /resourcemonitor and NAS temps/uptime info

--- syno_bot/modules/sys_info.py
+++ syno_bot/modules/sys_info.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 import time
 
-from synology_api.sys_info import SysInfo
+from synology_api.sys_info import SysInfo, DSM, Storage
 from syno_bot import dispatcher, NAS_IP, NAS_PORT, DSM_ACCOUNT, DSM_PASSWORD
 from syno_bot.modules.helper.bot_decorator import send_typing_action
 from syno_bot.modules.helper.file_size import human_readable_size
@@ -9,13 +10,15 @@ from syno_bot.modules.helper.string_processor import escape_reserved_character
 from telegram import ParseMode
 from telegram.ext import CommandHandler
 
-instance = SysInfo.login(DSM_ACCOUNT, DSM_PASSWORD, NAS_IP, NAS_PORT)
+sys_info = SysInfo.login(DSM_ACCOUNT, DSM_PASSWORD, NAS_IP, NAS_PORT)
+dsm = DSM.login(DSM_ACCOUNT, DSM_PASSWORD, NAS_IP, NAS_PORT)
+strg = Storage.login(DSM_ACCOUNT, DSM_PASSWORD, NAS_IP, NAS_PORT)
 
 @user_owner
 @send_typing_action
 def __nas_network_status(update, context):
-    network_status = instance.network_status()
-    vpn_status = instance.network_vpn_pptp()  # we can add the openVPN checks
+    network_status = sys_info.network_status()
+    vpn_status = sys_info.network_vpn_pptp()  # we can add the openVPN checks
     server_name = network_status["data"]["server_name"]
     dns_primary = network_status["data"]["dns_primary"]
     dns_secondary = network_status["data"]["dns_secondary"]
@@ -41,9 +44,57 @@ def __nas_network_status(update, context):
 @user_owner
 @send_typing_action
 def __nas_health_status(update, context):
-    util = instance.utilisation()["data"]
-    storage = instance.storage()["data"]
+    dsmInfo = dsm.get_info()["data"]
+    storage = strg.storage()["data"]
     print(str(storage))
+
+    temperature = dsmInfo["temperature"]
+    temperatureWarn = dsmInfo["temperature_warn"] == "True"
+    reply_text = "*NAS Temperatures*\n"
+    reply_text += "`Temperature     : {}°C`\n".format(temperature)
+    reply_text += "`Condition       : {}`\n".format("Warning! Check DSM for more info!" if temperatureWarn else "Good")
+
+    reply_text += "\n*Storage Volume*\n"
+    volumes = storage["volumes"]
+    for volume in volumes:
+        total = int(volume["size"]["total"])
+        used = int(volume["size"]["used"])
+        available = total - used
+        reply_text += "`{}`\n".format(volume["id"])
+        reply_text += "`Total     : {}`\n".format(human_readable_size(total))
+        reply_text += "`Used      : {}`\n".format(human_readable_size(used))
+        reply_text += "`Available : {}`\n\n".format(human_readable_size(available))
+
+    reply_text += "*Uptime*\n"
+    uptime_seconds = float(dsmInfo["uptime"])
+    uptime_day = uptime_seconds // (24 * 3600)
+    uptime_seconds = uptime_seconds % (24 * 3600)
+    uptime_hour = uptime_seconds // 3600
+    uptime_seconds %= 3600
+    uptime_minutes = uptime_seconds // 60
+    uptime_seconds %= 60
+    seconds = uptime_seconds
+
+    processed_date_reply = str()
+    if (uptime_day == 1):
+        processed_date_reply = "{0} day ".format(int(uptime_day))
+    elif (uptime_day > 0):
+        processed_date_reply = "{0} days ".format(int(uptime_day))
+
+    reply_text += ("`{0}{1}:{2}:{3}`".format(processed_date_reply, \
+        "{0:0=2d}".format(int(uptime_hour)), "{0:0=2d}".format(int(uptime_minutes)), \
+        "{0:0=2d}".format(int(seconds))))
+
+    update_time = time.strftime("%d %B %Y %H:%M:%S", time.localtime(time.time()))
+    reply_text += "\n\nLast update: {}".format(update_time)
+
+    update.message.reply_text(text=escape_reserved_character(reply_text),
+                             parse_mode=ParseMode.MARKDOWN_V2)
+
+@user_owner
+@send_typing_action
+def __resource_monitor(update, context):
+    util = sys_info.utilisation()["data"]
 
     user_load = util["cpu"]["user_load"]
     system_load = util["cpu"]["system_load"]
@@ -82,23 +133,11 @@ def __nas_health_status(update, context):
             name_padded += " "
         reply_text += "`{0}: {1}%`\n".format(name_padded, util)
 
-    reply_text += "\n*Storage Utilization*\n"
-    volumes = storage["volumes"]
-    for volume in volumes:
-        total = int(volume["size"]["total"])
-        used = int(volume["size"]["used"])
-        available = total - used
-        reply_text += "`{}`\n".format(volume["id"])
-        reply_text += "`Total     : {}`\n".format(human_readable_size(total))
-        reply_text += "`Used      : {}`\n".format(human_readable_size(used))
-        reply_text += "`Available : {}`\n\n".format(human_readable_size(available))
-
     update_time = time.strftime("%d %B %Y %H:%M:%S", time.localtime(time.time()))
-    reply_text += "Last update: {}".format(update_time)
+    reply_text += "\nLast update: {}".format(update_time)
 
     update.message.reply_text(text=escape_reserved_character(reply_text),
                              parse_mode=ParseMode.MARKDOWN_V2)
-
 
 @user_owner
 @send_typing_action
@@ -107,10 +146,12 @@ def __bot_health_status(update, context):
 
 
 nas_network_handler = CommandHandler("nasnetwork", __nas_network_status)
+resource_monitor_handler = CommandHandler("resourcemonitor", __resource_monitor)
 nas_health_handler = CommandHandler("nashealth", __nas_health_status)
 bot_health_handler = CommandHandler("bothealth", __bot_health_status)
 
 dispatcher.add_handler(nas_network_handler)
+dispatcher.add_handler(resource_monitor_handler)
 dispatcher.add_handler(nas_health_handler)
 dispatcher.add_handler(bot_health_handler)
 
